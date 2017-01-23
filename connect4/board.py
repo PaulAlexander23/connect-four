cca2ef6c21ac50a20895d1dc15cc81666b0ad167
Alexander <jpa13@ic.ac.uk>
2017-01-23 16:17:06 +0000

    Added folders, __init__.py and class board.py

---
+++ connect4/board.py
@@ -0,0 +1,12 @@
+"""Comments
+
+more comments
+"""
+
+class board:
+    def __init__(self):
+        self.cell = []
+        for i in range(43):
+            self.cell.append(0)
+                
+                
