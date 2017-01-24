1aacf516b4555e2774159253f89a6ca64801ab2e
Alexander <jpa13@ic.ac.uk>
2017-01-24 13:13:11 +0000

    Added drop and display functions to board

--- connect4/board.py
+++ connect4/board.py
@@ -8,5 +8,17 @@ class board:
         self.cell = []
         for i in range(43):
             self.cell.append(0)
-                
-                
+    
+    def drop(self, column, player):
+        for j in range(7):
+            if self.cell[column+j*6] == 0:
+                self.cell[column+j*6] = player
+                return 0;
+        return 1
+    
+    def display(self):
+        for i in range(8):
+            for j in range(7):
+                print(self.cell[i+(6-j)*6])
+            print("\n")
+        return 0
